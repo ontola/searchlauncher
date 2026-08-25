a33a770ee286c9995216045c693e53a397eb70ff
Joep Meindertsma <joep@ontola.io>
2026-08-25 21:22:24 +0200

    Release 0.0.21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
     // F-Droid greps these two literals out of this file to notice new release tags, so
     // they have to stay plain literals and be bumped in the commit that gets tagged. The series
     // starts at 250 to clear 242, the highest the old commit-count scheme ever shipped.
-    versionCode = 258
-    versionName = "0.0.20"
+    versionCode = 259
+    versionName = "0.0.21"
 
     buildConfigField("String", "GIT_HASH", "\"$gitHash\"")
     buildConfigField("String", "BUILD_DATE", "\"$buildDate\"")
