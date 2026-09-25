dcf5a68d13b0aa06db357f95ac02f89e40ef3941
Joep Meindertsma <joep@ontola.io>
2026-09-25 19:03:24 +0200

    Release 0.0.46: set alarms from search, shorter onboarding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
     // F-Droid greps these two literals out of this file to notice new release tags, so
     // they have to stay plain literals and be bumped in the commit that gets tagged. The series
     // starts at 250 to clear 242, the highest the old commit-count scheme ever shipped.
-    versionCode = 283
-    versionName = "0.0.45"
+    versionCode = 284
+    versionName = "0.0.46"
 
     buildConfigField("String", "GIT_HASH", "\"$gitHash\"")
     buildConfigField("String", "BUILD_DATE", "\"$buildDate\"")
